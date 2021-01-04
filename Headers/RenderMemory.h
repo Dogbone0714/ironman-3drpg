38a4ddc2cdd785ab95ebdffe5449a635d3491b5a
Dogbone0714 <jameskang0714@gmail.com>
2021-01-04 22:52:32 +0800

    Update RenderMemory.h

---
+++ Headers/RenderMemory.h
@@ -0,0 +1,8 @@
+/* File: RenderMemory.h */
+
+#ifndef __RENDERMEMORY_H__  // 怕別的標頭檔的定義互撞而加註，習慣是寫標頭檔都會加。
+#define __RENDERMEMORY_H__  // 命名潛規則：兩次底線+全大寫檔名+一次底線+大寫H+兩次底線
+
+
+
+#endif // __RENDERMEMORY_H__
