7244c2ac3ed110724c710e69927b178103f8df40
Dogbone0714 <jameskang0714@gmail.com>
2021-01-04 23:06:11 +0800

    Update Main.c

--- Sources/Main.c
+++ Sources/Main.c
@@ -1,13 +1,14 @@
 /* File: Main.c */
 
-#include <stdio.h>  // 標準輸入輸出
-#include <stdlib.h>  // 標準函式庫
-#include <windows.h>  // 控制視窗用
-#include <math.h>  // 數學函式庫(三角函數、四捨五入等等)
+#include <stdio.h>  
+#include <stdlib.h>  
+#include <windows.h>  
+#include <math.h>  
+#include "RenderMemory.h"
 
 int main(int argc, char *argv[])
 {
-
+    init_render_memory();
 
     return 0;
 }
