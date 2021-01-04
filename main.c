0fb4c85bb0de31cfb6780ac853081b21ecf94f2d
Dogbone0714 <jameskang0714@gmail.com>
2021-01-04 22:41:00 +0800

    Update main.c

--- main.c
+++ main.c
@@ -2,16 +2,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-double esd;  // 眼睛到螢幕的距離(Eyes to Screen Distance)，也稱視野距離
+double esd = 0.0;
 
-int main()
+int projectTo2d_x(double x, double y, double z)
 {
-    double x_3d = 0.0;  // 原三維座標X分量
-    double y_3d = 0.0;  // 原三維座標Y分量
-    double z_3d = 0.0;  // 原三維座標Z分量
+    double ax = x;
+    double eod = z;
+    double result = ax * (esd / eod);
+    return round(result);
+}
 
-    int bx = 0;  // 轉換後二維座標的X座標(螢幕上座標必須使用整數，文字的像素只有整數個)
-    int by = 0;  // 轉換後二維座標的Y座標(同上)
+int projectTo2d_y(double x, double y, double z)
+{
+    double ay = y;
+    double eod = z;
+    double result = ay * (esd / eod);
+    return round(result);
+}
+
+int main(int argc, char *argv[])
+{
+    double x_3d = 0.0;
+    double y_3d = 0.0;
+    double z_3d = 0.0;
+
+    int bx = 0;
+    int by = 0;
+
+    esd = 10.0;
+
+    for(;;)
+    {
+        printf("輸入3D座標：\n");
+        scanf("%lf %lf %lf", &x_3d, &y_3d, &z_3d);
+
+        bx = projectTo2d_x(x_3d, y_3d, z_3d);
+        by = projectTo2d_y(x_3d, y_3d, z_3d);
+
+        printf("原3D座標：(%f, %f, %f)\n", x_3d, y_3d, z_3d);
+        printf("轉換後2D座標：(%d, %d)\n", bx, by);
+    }
 
     return 0;
 }
