ef3bd395c8bb727a66ffb588cb63466be0675337
Dogbone0714 <jameskang0714@gmail.com>
2021-01-05 09:05:52 +0800

    早9好痛苦

--- Sources/Main.c
+++ Sources/Main.c
@@ -7,13 +7,16 @@
 #include "RenderMemory.h"
 #include "SystemSetting.h"
 #include "GameStatus.h"
+#include "Renderer.h"  
+
+#define M_PI acos(-1.0)
 
 int main(int argc, char *argv[])
 {
     init_render_memory();
     setting_system();
     initGameStatus();  // 初始化遊戲狀態(畫面是否更新初始化為True)
-
+    init_renderer();
     
     return 0;
 }
