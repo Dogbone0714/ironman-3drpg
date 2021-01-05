a31092dcf677e74cf64fd0f4d73930ebe57eb038
Dogbone0714 <jameskang0714@gmail.com>
2021-01-05 09:17:40 +0800

    ~

--- Headers/Renderer.h
+++ Headers/Renderer.h
@@ -6,6 +6,25 @@
 #define _CLEAN_MODE_ 0  // 底下render_screen的參數所需，事先定義好
 #define _RENDER_MODE_ 1
 
+typedef struct cube  // 定義方塊
+{
+    struct cube *next;  // 指向下一塊方塊
+    char style;  // 以下屬性是render_cube函數所需參數，
+    int cx;      // 同時也是繪製方塊所需的資料。
+    int cy;      // 我們可以在玩家按下"m"的時候將方塊資訊加入到linklist中，
+    int cz;      // 此方塊就正式「加入」這個世界，
+    int left;    // 因為往後的每次render_screen都會將linklist中所有方塊繪製出來。
+    int right;
+    int top;
+    int bottom;
+    int f;
+    int r;
+}Cube;
+
+Cube allCubesHead;  // linklist的頭，標的物
+Cube *allCubes;     // Cube指標，用來依序指向每一塊方塊
+Cube newCube;       // 遊戲開始時預設在世界中的方塊，也就是類似以前的「大平台」
+
 float camera_x_pos, camera_y_pos, camera_z_pos;  // 攝影機(大家的螢幕)位置
 float camera_speed;  // 相機移動速度，決定按下同樣按鍵時間攝影機移動快慢
 float fov;  // 視野，等同於前幾天所說的esd(眼睛到螢幕距離)
